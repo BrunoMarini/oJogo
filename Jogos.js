d4f4ddbc22333df14fcb25babb9b3a0f310e798d
Bruno Guilherme Spirlandeli Marini <bgmarini@hotmail.com>
2019-11-05 15:12:56 -0300

    Add files via upload

--- Jogos.js
+++ Jogos.js
@@ -1,6 +1,33 @@
 const Apostas = require("./Apostas.js");
 const Cartas = require("./Cartas.js");
-
+const chancesWinWheel chances = [
+			{“premio”:   50, “chance”: 5},
+			{“premio”:  100, “chance”: 5},
+			{“premio”:  200, “chance”: 5},
+			{“premio”:  300, “chance”: 5},
+			{“premio”:  400, “chance”: 5},
+			{“premio”:  100, “chance”: 5},
+			{“premio”:  200, “chance”: 5},
+			{“premio”:  300, “chance”: 5},
+			{“premio”:  400, “chance”: 5},
+			{“premio”:  500, “chance”: 1},
+			{“premio”:  600, “chance”: 1},
+			{“premio”:    0, “chance”: 1},
+			
+			{“premio”:   50, “chance”: 1},
+			{“premio”:  100, “chance”: 1},
+			{“premio”:  200, “chance”: 5},
+			{“premio”:  300, “chance”: 5},
+			{“premio”:  400, “chance”: 5},
+			{“premio”:  100, “chance”: 5},
+			{“premio”:  200, “chance”: 5},
+			{“premio”:  300, “chance”: 5},
+			{“premio”:  400, “chance”: 5},
+			{“premio”:  500, “chance”: 5},
+			{“premio”: 1000, “chance”: 5},
+			{“premio”:    0, “chance”: 5},
+		];
+		
 class Jogo {
     constructor() {
         this._jogadores = {};
@@ -80,4 +107,16 @@ class Blackjack extends Jogo {
         // Puxar cartas para o crupie ate que soma > 16
     }
 }
-// =================================  END BLACKJACK  =================================
+
+// ================================= BEGIN WIN WHEEL =================================
+class WinWheel extends Jogo {
+	constructor(){
+		
+		
+
+		
+	}
+	
+}
+// =================================  END WIN WHEEL  =================================
+
