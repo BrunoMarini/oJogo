a7805ac98772a55214ee542b7e6b7dd9440d1616
Caio Lima <caiotsan+github@gmail.com>
2019-11-05 15:34:29 -0300

    Cleanup

--- teste.js
+++ teste.js
@@ -36,3 +36,36 @@ console.log(M.acessarMesa("roleta", 0)._jogadores)
 M.acessarMesa("roleta", 0).calcularRodada();
 console.log("Depois:")
 console.log(M.acessarMesa("roleta", 0)._jogadores)
+// =====================================================
+let result = 0/360;
+let orig = result * 360;
+let pos = -1;
+const chances = [
+    {"premio":   50, "chance": 5},
+    {"premio":  100, "chance": 5},
+    {"premio":  200, "chance": 5},
+    {"premio":  300, "chance": 5},
+    {"premio":  400, "chance": 5},
+    {"premio":  100, "chance": 5},
+    {"premio":  200, "chance": 5},
+    {"premio":  300, "chance": 5},
+    {"premio":  400, "chance": 5},
+    {"premio":  500, "chance": 1},
+    {"premio":  600, "chance": 1},
+    {"premio":    0, "chance": 1},
+    
+    {"premio":   50, "chance": 1},
+    {"premio":  100, "chance": 1},
+    {"premio":  200, "chance": 5},
+    {"premio":  300, "chance": 5},
+    {"premio":  400, "chance": 5},
+    {"premio":  100, "chance": 5},
+    {"premio":  200, "chance": 5},
+    {"premio":  300, "chance": 5},
+    {"premio":  400, "chance": 5},
+    {"premio":  500, "chance": 5},
+    {"premio": 1000, "chance": 5},
+    {"premio":    0, "chance": 5},
+];
+while (result > 0) { result -= chances[++pos]['chance'] / 100.0; }
+console.log("Result = " + orig + "; pos = " + pos + "; valor = " + chances[pos]['premio']);
