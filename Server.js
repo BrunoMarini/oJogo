57672cd787601d85dfef3086e7a2e1ebc5db881a
Linksya <caiosanches@live.com>
2019-12-01 21:57:58 -0300

    Adding reference to images

--- Server.js
+++ Server.js
@@ -227,6 +227,8 @@ app.all('/dartsDOM.js', function(req, res){
     res.sendFile(fetchFile("/scripts/dartsDOM.js"));
 });
 app.all('/dartsStyle.css', (req, res) => { res.sendFile(fetchFile("/styles/dartsStyle.css")); });
+app.all('/darts/dardo.png', (req, res) => { res.sendFile(fetchFile("/recursos/imagens/darts/dardo.jpg")); });
+app.all('/darts/target.png', (req, res) => { res.sendFile(fetchFile("/recursos/imagens/darts/target.jpg")); });
 /* Fim Darts */
 
 /* Inicio Manager */
